d0f72297f20137bd6ad34e9ec4e62ae1697e398c
JipingZhang02 <jiz218@ucsd.edu>
2024-02-11 21:17:05 -0800

    fix bug in get attr

--- src/main/java/edu/ucsd/cse232b/expression/singleExpr/AttrExpr.java
+++ src/main/java/edu/ucsd/cse232b/expression/singleExpr/AttrExpr.java
@@ -1,6 +1,8 @@
 package edu.ucsd.cse232b.expression.singleExpr;
 
 import edu.ucsd.cse232b.expression.Expression;
+import edu.ucsd.cse232b.util.Util;
+import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
 import java.util.ArrayList;
@@ -10,13 +12,24 @@ public class AttrExpr implements Expression {
     private final String attrName;
 
     public AttrExpr(String attrName) {
+        if (attrName.startsWith("@")){
+            attrName = attrName.substring(1);
+        }
         this.attrName = attrName;
     }
 
     @Override
     public List<Node> evaluate(List<Node> inputNodes) throws Exception {
+        List<Node> i2 = new ArrayList<>();
+        for(Node node:inputNodes){
+            Node parent = node.getParentNode();
+            if (!(parent instanceof Document)){
+                i2.add(parent);
+            }
+        }
+        i2 = Util.removeRedaduntElements(i2);
         List<Node> res = new ArrayList<>();
-        for (Node node:inputNodes){
+        for (Node node:i2){
             Node attrNode = node.getAttributes().getNamedItem(attrName);
             if (attrNode!=null){
                 res.add(attrNode);
